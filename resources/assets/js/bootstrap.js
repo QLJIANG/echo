ebd10a51e87e8b093085bf51c505d41467bf8b68
qljiang <jiang_ql@126.com>
2017-04-15 19:20:12 +0800

    实现了实时聊天功能

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -41,9 +41,11 @@ window.axios.defaults.headers.common = {
 
 import Echo from "laravel-echo"
 
+window.Pusher = require('pusher-js');
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: window.Laravel.pusher,
-    cluster: 'eu',
+    cluster: 'ap1',
     encrypted: true
 });
