7ce76bfa2c838a1c06118a96e2665f76a360adca
qljiang <jiang_ql@126.com>
2017-04-18 01:06:53 +0800

    1

--- resources/assets/js/components/ChatScreen.vue
+++ resources/assets/js/components/ChatScreen.vue
@@ -36,10 +36,10 @@
             Echo.channel('messages').listen('.messagePosted', (data) => {
                 this.messages.push(data);
             });
-            // $('input[type="text"]').on('click', function () {
-            //     $("body").scrollTop(10);
-            //     console.log('click');
-            // });
+            $('input[type="text"]').on('click', function () {
+                $("body").scrollTop(10);
+                $(".chat-history").scrollTop(document.body.scrollHeight);
+            });
         },
         updated() {
             $(".chat-history").scrollTop(document.body.scrollHeight);
